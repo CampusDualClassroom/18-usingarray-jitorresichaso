c0eedecda0df70f5c9566d09e2c2c3a083e75a61
Juan Ignacio Torres Ichaso <jitorresichaso@macrotest.es>
2025-11-06 10:44:43 +0100

    Exercise 18 solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,7 +2,27 @@ package com.campusdual.classroom;
 
 public class Exercise {
 
-    public static void main(String[] args) {
+    public static int[] createAndInitializeArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
 
+    public static void showInlineArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = createAndInitializeArray(5);
+        showInlineArray(numbers);
     }
 }
+
